c4256a423066872fbe7340b27ec84d64ca4070b2
chejf <21575602@qq.com>
2020-06-10 20:04:19 +0800

    V

--- winDriver_libwin32/发布dll.bat
+++ winDriver_libwin32/发布dll.bat
@@ -5,7 +5,7 @@ copy SpectraArsenalForGCS_V5.02\Release\SpectraArsenal.lib .\libx32\lib
 copy SpectraArsenalForGCS_V5.02\SpectraArsenal.h .\libx32
 
 copy .\libx32\lib\*.dll windemon\Release\
-xcopy /y /c /h /r .\libx32\lib\*.h windemon\
+xcopy /y /c /h /r .\libx32\*.h windemon\
 xcopy /y /c /h /r .\libx32\lib\*.lib windemon\
 
 del /F /A /Q .\libx64\lib\*.*
@@ -15,6 +15,6 @@ copy SpectraArsenal64\x64\Release\SpectraArsenal.lib .\libx64\lib
 copy SpectraArsenal64\SpectraArsenal\SpectraArsenal.h .\libx64
 
 copy .\libx64\lib\*.dll 64demon1\64demon1\
-xcopy /y /c /h /r .\libx64\lib\*.h 64demon1\64demon1\
+xcopy /y /c /h /r .\libx64\*.h 64demon1\64demon1\
 xcopy /y /c /h /r .\libx64\lib\*.lib 64demon1\64demon1\
 pause
