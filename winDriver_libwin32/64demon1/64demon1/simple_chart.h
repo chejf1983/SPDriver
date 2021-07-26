eb71295d641e430e9b42400f07cee7e6f56c1e36
chejf <21575602@qq.com>
2021-07-26 16:25:12 +0800

    删除过时文件

--- winDriver_libwin32/64demon1/64demon1/simple_chart.h
+++
@@ -1,3 +0,0 @@
-
-void print_chart(double x[], double y[], int data_len);
-void print_chart(double y[], int data_len);
--- winDriver_libwin32/windemon/spectraltest.h
+++
@@ -1,9 +0,0 @@
-void initDll();
-void GetAPIVersion();
-void OpenSpectrometers();
-void GetSpectrometersName();
-void SetIntegrationTime();
-void GetSpectum();
-void AsyGetSpectum();
-void CloseSpectrometers();		
-void MultiChanneTest();
